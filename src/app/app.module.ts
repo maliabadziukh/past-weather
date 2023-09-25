ade44137deac3cd0a8cf6215278106b476c93cfb
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2023-09-25 21:12:19 +0200

    basic app setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { WeatherDisplayComponent } from './weather-display/weather-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputComponent,
+    WeatherDisplayComponent
   ],
   imports: [
     BrowserModule
