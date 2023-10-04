0af1e5b18ce44877fee154f6581d18461610c8bb
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2023-10-04 12:48:23 +0200

    chore: update packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { CityInputComponent } from '@molecules';
+import { CityInputComponent, DateInputComponent } from '@molecules';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { InputCardComponent, WeatherDisplayComponent } from '@organisms';
 
 import { AppComponent } from './app.component';
-import { DateInputComponent } from './components/molecules/date-input/date-input.component';
-import { InputCardComponent } from './components/organisms/input-card/input-card.component';
-import { WeatherDisplayComponent } from './components/organisms/weather-display/weather-display.component';
 import { cityReducer } from './store/reducers/weather-app.reducer';
 @NgModule({
   declarations: [AppComponent, InputCardComponent, WeatherDisplayComponent, CityInputComponent, DateInputComponent],
