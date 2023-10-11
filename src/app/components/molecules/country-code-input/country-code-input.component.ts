8b2ea732162c0c6c6fc30a60d5ddc587cbe026b5
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2023-10-11 16:59:43 +0200

    feat(488084): add searchable countryCode dropdown

--- src/app/components/molecules/country-code-input/country-code-input.component.ts
+++ src/app/components/molecules/country-code-input/country-code-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+import { CountriesService } from 'src/app/services/countries.service';
 
 @Component({
   selector: 'app-country-code-input',
@@ -9,9 +10,13 @@ import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 export class CountryCodeInputComponent implements OnInit {
   countryCode: AbstractControl;
   @Input() parentForm: FormGroup;
+  countryOptions: { name: string; code: string }[];
+
+  constructor(private countriesService: CountriesService) {}
 
   ngOnInit(): void {
     this.countryCode = new FormControl('');
     this.parentForm.addControl('countryCode', this.countryCode);
+    this.countryOptions = this.countriesService.getCountries();
   }
 }
