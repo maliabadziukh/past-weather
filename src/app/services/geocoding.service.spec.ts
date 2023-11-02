9c3135638f0655e505e81c8a2d1ef75033146063
Malia Badziukh <84846526+maliabadziukh@users.noreply.github.com>
2023-11-02 15:54:21 +0100

    test(487775): add unit tests for services

--- src/app/services/geocoding.service.spec.ts
+++ src/app/services/geocoding.service.spec.ts
@@ -1,16 +1,43 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { GeocodingService } from './geocoding.service';
 
 describe('GeocodingService', () => {
   let service: GeocodingService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(GeocodingService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should make an HTTP GET request with correct params', () => {
+    const mockResponse = {
+      name: 'test',
+      country: 'test',
+      lat: 123,
+      lon: 456,
+    };
+    const testCity = 'city';
+    const testCountryCode = 'cc';
+
+    service.getLocation(testCity, testCountryCode).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpTestingController.expectOne(request => {
+      return (
+        request.url === service['apiUrl'] &&
+        request.method === 'GET' &&
+        request.params.get('q') === testCity + ',' + testCountryCode &&
+        request.params.get('appid') === service['apiKey']
+      );
+    });
+
+    req.flush(mockResponse);
+
+    httpTestingController.verify();
   });
 });
